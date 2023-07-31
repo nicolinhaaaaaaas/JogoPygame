193f16bd7e4160af227890eaee922b29d654e61f
nicolinhaaaaaaas <112559599+nicolinhaaaaaaas@users.noreply.github.com>
2023-07-31 14:05:30 -0300

    update

--- Tela.py
+++ Tela.py
@@ -11,7 +11,9 @@ test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('graphics/sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
-text_surface = test_font.render('Meu Jogo', False, 'Black')
+
+score_surface = test_font.render('Meu Jogo', False, 'Black')
+score_rect = score_surface.get_rect(center = (400, 50))
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect = snail_surface.get_rect(bottomright = (600, 300))
@@ -25,11 +27,18 @@ while True: # tem que colocar no while true que vai durar pra sempre, pra tela n
         if event.type == pygame.QUIT:
             pygame.quit() #fechando
             exit()
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print('mouse down')
+        #if event.type == pygame.MOUSEMOTION:
+        #    if player_rect.collidepoint(event.pos): print ('collision')
 
 
     screen.blit(sky_surface, (0,0)) #isso ta colocando uma surface dentro da display surface
     screen.blit(ground_surface, (0, 300))
-    screen.blit( text_surface, (300, 50))
+
+    pygame.draw.rect(screen, 'Pink', score_rect)
+    pygame.draw.rect(screen, 'Pink', score_rect, 10)
+    screen.blit(score_surface, score_rect)
 
     snail_rect.x -= 4
     if snail_rect.right <= 0: snail_rect.left = 800
@@ -38,6 +47,17 @@ while True: # tem que colocar no while true que vai durar pra sempre, pra tela n
     player_rect.left += 1
     screen.blit(player_surface, player_rect)
     
+    #if player_rect.colliderect(snail_rect):
+    #    print('collision')
+
+    #mouse_pos = pygame.mouse.get_pos()
+    #if player_rect.collidepoint(mouse_pos):
+    #    print(pygame.mouse.get_pressed())
+
+
+
+
+
     # desenhar todos os elementos
     #atualizar tudo
     pygame.display.update()
